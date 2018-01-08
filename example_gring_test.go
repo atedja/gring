1c5dfb0b4de0a818f5bfbb014f8315e6893514db
Albert Tedja <albert@siliconaxon.com>
2018-01-07 23:32:22 -0800

    Added example

---
+++ example_gring_test.go
@@ -0,0 +1,21 @@
+package gring_test
+
+import (
+	"fmt"
+	"github.com/atedja/gring"
+)
+
+func ExampleRing() {
+	ring := gring.New()
+	ring.AddWithValue(1)
+	ring.AddWithValue(10)
+	ring.AddWithValue(100)
+
+	values := make([]int, 0)
+	iter, _ := ring.Iterator()
+	for iter.Next() {
+		values = append(values, iter.Value().(int))
+	}
+
+	fmt.Println(values)
+}
